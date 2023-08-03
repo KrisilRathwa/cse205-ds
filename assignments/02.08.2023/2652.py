17a3eef7607c5a64a0c3d02a063261b4eb11edd8
Krisil Rathwa <krisil.r@ahduni.edu.in>
2023-08-03 13:33:02 +0530

    assignments

---
+++ assignments/02.08.2023/2652.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def sumOfMultiples(self, n):
+        sum_all=0
+        for i in range(1,n+1):
+            if (i%3==0 or i%5==0 or i%7==0):
+                sum_all=sum_all+i
+        return sum_all
